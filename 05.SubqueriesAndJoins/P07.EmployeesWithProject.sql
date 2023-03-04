83756300e68c45c885a1ab61b5710de0bb1ee82e
Dimitar Vasilev <captaingetrekt@gmail.com>
2023-03-04 15:47:06 +0200

    Create P07.EmployeesWithProject.sql

---
+++ 05.SubqueriesAndJoins/P07.EmployeesWithProject.sql
@@ -0,0 +1,9 @@
+SELECT TOP 5 e.EmployeeID, e.FirstName, p.[Name] AS 'ProjectName'
+FROM Employees AS e
+	INNER JOIN EmployeesProjects AS ep
+	ON ep.EmployeeID = e.EmployeeID
+		INNER JOIN Projects AS p
+		ON p.ProjectID = ep.ProjectID
+WHERE p.StartDate > '2002-08-13' 
+	AND EndDate IS NULL
+ORDER BY e.EmployeeID ASC;
