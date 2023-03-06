df5915ed1f161d9d5616cfee776bab2cf3c33851
Dimitar Vasilev <captaingetrekt@gmail.com>
2023-03-06 19:18:57 +0200

    Create P02.EmployeesWithSalaryAboveNumber.sql

---
+++ 07.FunctionsAndStoredProcedures/P02.EmployeesWithSalaryAboveNumber.sql
@@ -0,0 +1,9 @@
+CREATE OR ALTER PROC usp_GetEmployeesSalaryAboveNumber @MinSalary DECIMAL(18, 4)
+AS
+BEGIN
+	SELECT FirstName, LastName
+	FROM Employees
+	WHERE Salary >= @MinSalary
+END
+GO
+EXEC usp_GetEmployeesSalaryAboveNumber 48100;
