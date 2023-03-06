8ce02c5962a09bab748cd9a9fb9c9d8805550f43
Dimitar Vasilev <captaingetrekt@gmail.com>
2023-03-06 19:18:54 +0200

    Create P01.EmployeesWithSalaryAbove35000.sql

---
+++ 07.FunctionsAndStoredProcedures/P01.EmployeesWithSalaryAbove35000.sql
@@ -0,0 +1,11 @@
+USE SoftUni;
+
+CREATE OR ALTER PROC usp_GetEmployeesSalaryAbove35000
+AS
+BEGIN
+	SELECT e.FirstName, e.LastName
+	FROM Employees AS e
+	WHERE e.Salary > 35000
+END
+GO
+EXEC usp_GetEmployeesSalaryAbove35000;
