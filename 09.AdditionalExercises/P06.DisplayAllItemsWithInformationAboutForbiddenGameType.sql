3f4fa6939cdbaf44f6befad05914c200f3e469dd
Dimitar Vasilev <captaingetrekt@gmail.com>
2023-03-09 21:39:21 +0200

    Create P06.DisplayAllItemsWithInformationAboutForbiddenGameType.sql

---
+++ 09.AdditionalExercises/P06.DisplayAllItemsWithInformationAboutForbiddenGameType.sql
@@ -0,0 +1,8 @@
+SELECT i.[Name] AS 'Item', i.Price, i.MinLevel,
+	gt.[Name] AS 'Forbidden Game Type'
+FROM Items AS i
+	LEFT OUTER JOIN GameTypeForbiddenItems AS gtfi
+	ON gtfi.ItemId = i.Id
+		LEFT OUTER JOIN GameTypes AS gt
+		ON gt.Id = gtfi.GameTypeId
+ORDER BY [Forbidden Game Type] DESC, [Item] ASC;
