b85d40a012828fedc80ea318c7b6692d7e955160
Dimitar Vasilev <captaingetrekt@gmail.com>
2023-03-09 21:39:34 +0200

    Create P11.CountOfCountriesByCurrency.sql

---
+++ 09.AdditionalExercises/P11.CountOfCountriesByCurrency.sql
@@ -0,0 +1,7 @@
+SELECT cu.CurrencyCode, cu.[Description] AS 'Currency',
+	COUNT(co.CountryCode) AS 'NumberOfCountries'
+FROM Currencies AS cu
+	INNER JOIN Countries AS co
+	ON co.CurrencyCode = cu.CurrencyCode
+GROUP BY cu.CurrencyCode, cu.[Description]
+ORDER BY NumberOfCountries DESC, Currency ASC;
