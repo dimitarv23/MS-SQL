d2cf4f1a915ec14e3d9727ff5477cecdcaecf2c8
Dimitar Vasilev <captaingetrekt@gmail.com>
2023-03-11 15:43:43 +0200

    Create P09.TouristsWithTheirBonusPrizes.sql

---
+++ Exams/Exam Prep 1/P09.TouristsWithTheirBonusPrizes.sql
@@ -0,0 +1,12 @@
+SELECT t.[Name], t.Age, t.PhoneNumber, 
+	t.Nationality, CASE 
+		WHEN bp.[Name] IS NULL 
+			THEN '(no bonus prize)'
+		ELSE bp.[Name]
+		END AS 'Reward'
+FROM Tourists AS t
+	LEFT OUTER JOIN TouristsBonusPrizes AS tbp
+	ON tbp.TouristId = t.Id
+		LEFT OUTER JOIN BonusPrizes AS bp
+		ON bp.Id = tbp.BonusPrizeId
+ORDER BY t.[Name] ASC;
