5f4e137664836e6bd8bc067fbe174850d3804bd8
Dimitar Vasilev <captaingetrekt@gmail.com>
2023-03-11 15:43:47 +0200

    Create P10.TouristsVisitingHistoryAndArchaeologySites.sql

---
+++ Exams/Exam Prep 1/P10.TouristsVisitingHistoryAndArchaeologySites.sql
@@ -0,0 +1,14 @@
+SELECT DISTINCT SUBSTRING(t.[Name], 
+	CHARINDEX(' ', t.[Name]) + 1, 
+	LEN(t.[Name]) - CHARINDEX(' ', t.[Name])
+	) AS 'LastName', t.Nationality, t.Age,
+	t.PhoneNumber
+FROM Tourists AS t
+	INNER JOIN SitesTourists AS st
+	ON st.TouristId = t.Id
+		INNER JOIN Sites AS s
+		ON s.Id = st.SiteId
+		INNER JOIN Categories AS c
+		ON c.Id = s.CategoryId
+WHERE c.[Name] = 'History and archaeology'
+ORDER BY LastName ASC;
