be77f4bcb78b5ba64243b2cac77fd762161b96c7
Dimitar Vasilev <captaingetrekt@gmail.com>
2023-03-12 09:40:11 +0200

    Create P04.Delete.sql

---
+++ Exams/Exam Prep 2/P04.Delete.sql
@@ -0,0 +1,9 @@
+DELETE FROM Volunteers
+WHERE DepartmentId = (
+	SELECT Id
+	FROM VolunteersDepartments
+	WHERE DepartmentName = 'Education program assistant'
+	);
+
+DELETE FROM VolunteersDepartments
+WHERE DepartmentName = 'Education program assistant';
