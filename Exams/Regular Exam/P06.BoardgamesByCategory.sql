944bc89d31bfe2eb96812da1ab31eb1d3cf9538a
Dimitar Vasilev <captaingetrekt@gmail.com>
2023-03-14 18:40:48 +0200

    Create P06.BoardgamesByCategory.sql

---
+++ Exams/Regular Exam/P06.BoardgamesByCategory.sql
@@ -0,0 +1,7 @@
+SELECT b.Id, b.[Name], b.YearPublished, 
+	c.[Name] AS 'CategoryName'
+FROM Boardgames AS b
+	INNER JOIN Categories AS c
+	ON c.Id = b.CategoryId
+WHERE c.[Name] IN ('Strategy Games', 'Wargames')
+ORDER BY YearPublished DESC;
